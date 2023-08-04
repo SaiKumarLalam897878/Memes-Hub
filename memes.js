083b7f557c88757f0b476884e25d629c1b1bb074
SaiKumarLalam897878 <saikumarlalam12345@gmail.com>
2023-08-04 12:42:58 +0530

    navbar styles changed for small devices

--- memes.js
+++ memes.js
@@ -3,3 +3,16 @@ const showNavbar = document.querySelector(".navbar");
 window.addEventListener('scroll', () => {
     showNavbar.classList.toggle('show__background', window.scrollY > 100);
 });
+
+
+//small devices navbar
+
+const headerLinks = document.querySelectorAll(".header__link");
+const hamMenu = document.querySelector('.ham__menu');
+const hamMenuBar = document.querySelector('.ham__menu-bar');
+const headerUl = document.querySelector('.header__links');
+
+hamMenu.addEventListener("click", () => {
+    hamMenuBar.classList.toggle('fa-xmark');
+    headerUl.classList.toggle("ul__show");
+})
